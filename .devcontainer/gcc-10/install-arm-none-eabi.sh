9043a835200d4b7d4a9dc03d4c9d6a7ea60b5b9f
Ron Jaegers <ron.jaegers@philips.com>
2023-10-04 11:53:21 +0000

    chore: update dependencies
    - Update gcc-10 to gcc-12 for host and arm-none-eabi
    - Update bats from 1.9.0 to 1.10.0
    - update ccache from 4.8.2 to 4.8.3
    - update docker-cli from 24.0.4 to 24.0.6

--- .devcontainer/gcc-10/install-arm-none-eabi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-mkdir /opt/gcc-arm-none-eabi
-wget -qO - https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-$(uname -m)-linux.tar.bz2 | tar xj --strip-components=1 -C /opt/gcc-arm-none-eabi
