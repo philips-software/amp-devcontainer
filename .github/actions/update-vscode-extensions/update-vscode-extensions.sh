3a815a47e2169c7cf944719a9401ef21fa317e9d
Ron <45816308+rjaegers@users.noreply.github.com>
2024-03-05 13:59:34 +0000

    chore: fix comment layout

--- .github/actions/update-vscode-extensions/update-vscode-extensions.sh
+++ .github/actions/update-vscode-extensions/update-vscode-extensions.sh
@@ -19,7 +19,7 @@ for EXTENSION in $(echo $JSON | jq -r '.[].customizations.vscode.extensions | fl
         GITHUB_URL=$(echo $LATEST_NON_PRERELEASE_VERSION_JSON | jq -r '.properties | map(select(.key == "Microsoft.VisualStudio.Services.Links.GitHub"))[] | .value')
 
         RELEASE_DETAILS=$(gh release view --json body -R $GITHUB_URL | jq -r '.body')
-        UPDATE_DETAILS=$(printf "Updates \`%s\` from %s to %s\n<details><summary>Release notes</summary>%s</details>\n\n%s" $NAME $CURRENT_VERSION $LATEST_NON_PRERELEASE_VERSION "$RELEASE_DETAILS" "$UPDATE_DETAILS")
+        UPDATE_DETAILS=$(printf "Updates \`%s\` from %s to %s\n<details>\n<summary>Release notes</summary>\n\n%s\n</details>\n\n\n\n%s" $NAME $CURRENT_VERSION $LATEST_NON_PRERELEASE_VERSION "$RELEASE_DETAILS" "$UPDATE_DETAILS")
     fi
 
     EXTENSIONS="\"$NAME@$LATEST_NON_PRERELEASE_VERSION\",$EXTENSIONS"
