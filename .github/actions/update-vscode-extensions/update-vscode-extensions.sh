a33dd3c0666241bd545991d4b530c964cb5336c1
Ron <45816308+rjaegers@users.noreply.github.com>
2024-03-06 08:42:08 +0000

    chore: include releasenotes for all versions between latests and current

--- .github/actions/update-vscode-extensions/update-vscode-extensions.sh
+++ .github/actions/update-vscode-extensions/update-vscode-extensions.sh
@@ -7,6 +7,24 @@ JSON=$(cat $FILE)
 EXTENSIONS=
 UPDATE_DETAILS=
 
+get_github_releasenotes() {
+    local GITHUB_URL=${1:?}
+    local CURRENT_RELEASE=${2:?}
+
+    gh release list --exclude-drafts --exclude-pre-releases -R $GITHUB_URL --json name,tagName --jq '.[]' | while read -r RELEASE;
+    do
+        NAME=$(echo "$RELEASE" | jq -r '.name')
+        TAG=$(echo "$RELEASE" | jq -r '.tagName')
+
+        if [[ $NAME == $CURRENT_RELEASE ]];
+        then
+            break;
+        fi
+
+        printf "%s\n\n" "$(gh release view --json body --jq '.body' -R $GITHUB_URL $TAG)"
+    done
+}
+
 for EXTENSION in $(echo $JSON | jq -r '.[].customizations.vscode.extensions | flatten[]'); do
     NAME="${EXTENSION%%@*}"
     CURRENT_VERSION="${EXTENSION#*@}"
@@ -18,8 +36,8 @@ for EXTENSION in $(echo $JSON | jq -r '.[].customizations.vscode.extensions | fl
     then
         GITHUB_URL=$(echo $LATEST_NON_PRERELEASE_VERSION_JSON | jq -r '.properties | map(select(.key == "Microsoft.VisualStudio.Services.Links.GitHub"))[] | .value')
 
-        RELEASE_DETAILS=$(gh release view --json body -R $GITHUB_URL | jq '.body')
-        UPDATE_DETAILS=$(printf "Updates \`%s\` from %s to %s<br><details><summary>Release notes</summary><br><br>%s<br></details><br><br>%s" $NAME $CURRENT_VERSION $LATEST_NON_PRERELEASE_VERSION "$RELEASE_DETAILS" "$UPDATE_DETAILS")
+        RELEASE_DETAILS=$(get_github_releasenotes $GITHUB_URL $CURRENT_VERSION)
+        UPDATE_DETAILS=$(printf "Updates \`%s\` from %s to %s<br><details><summary>Release notes</summary><br><br><blockquote>%s</blockquote><br></details><br><br>%s" $NAME $CURRENT_VERSION $LATEST_NON_PRERELEASE_VERSION "$RELEASE_DETAILS" "$UPDATE_DETAILS")
     fi
 
     EXTENSIONS="\"$NAME@$LATEST_NON_PRERELEASE_VERSION\",$EXTENSIONS"
