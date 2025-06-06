ab914b15c1d23a3e364a5dd94cb8b9d97ef96eee
Ron <45816308+rjaegers@users.noreply.github.com>
2025-06-06 07:05:40 +0000

    chore: make container size diff more appealing

--- .github/actions/container-size-diff/container-size-diff.sh
+++ .github/actions/container-size-diff/container-size-diff.sh
@@ -23,16 +23,38 @@ get_sizes_from_manifest() {
 get_sizes_from_manifest ${FROM_CONTAINER} FROM_CONTAINER_SIZES
 get_sizes_from_manifest ${TO_CONTAINER} TO_CONTAINER_SIZES
 
-echo "## Compressed layer size comparison"
+echo "## 📊 Container Size Analysis"
 echo
-echo "Comparing \`${FROM_CONTAINER}\` to \`${TO_CONTAINER}\`"
+echo "Comparing compressed layer sizes of:"
+echo "📦 Base: \`${FROM_CONTAINER}\`"
+echo "📦 Current: \`${TO_CONTAINER}\`"
 echo
-echo "| OS/Platform | Previous Size | Current Size | Delta |"
-echo "|-------------|---------------|--------------|-------|"
+
+echo "### 📈 Size Comparison Table"
+echo
+echo "| OS/Platform | Previous Size | Current Size | Change | Trend |"
+echo "|-------------|:-------------:|:------------:|:------:|:-----:|"
+
 for PLATFORM in "${!FROM_CONTAINER_SIZES[@]}";
 do
     BASE_SIZE=${FROM_CONTAINER_SIZES[${PLATFORM}]}
     HEAD_SIZE=${TO_CONTAINER_SIZES[${PLATFORM}]}
+    DELTA=$((${HEAD_SIZE} - ${BASE_SIZE}))
+    PERCENT_CHANGE=$(python -c "print('{:+0.2f}'.format(((${HEAD_SIZE} - ${BASE_SIZE}) / ${BASE_SIZE}) * 100))")
 
-    echo "| ${PLATFORM} | $(numfmt --to iec --format '%.2f' ${BASE_SIZE}) | $(numfmt --to iec --format '%.2f' ${HEAD_SIZE}) | $(numfmt --to iec --format '%.2f' -- $((${HEAD_SIZE} - ${BASE_SIZE}))) $(python -c "print('({:+0.2f}%)'.format(((${HEAD_SIZE} - ${BASE_SIZE}) / ${BASE_SIZE}) * 100))") |";
+    if (( DELTA < 0 )); then
+        ICON="🔽"
+        MD_COLOR_START="<span style=\"color:green\">"
+        MD_COLOR_END="</span>"
+    elif (( DELTA > 0 )); then
+        ICON="🔼"
+        MD_COLOR_START="<span style=\"color:red\">"
+        MD_COLOR_END="</span>"
+    else
+        ICON="🔄"
+        MD_COLOR_START=""
+        MD_COLOR_END=""
+    fi
+
+    echo "| ${PLATFORM} | $(numfmt --to iec --format '%.2f' ${BASE_SIZE}) | $(numfmt --to iec --format '%.2f' ${HEAD_SIZE}) | ${MD_COLOR_START}$(numfmt --to iec --format '%.2f' ${DELTA}) (${PERCENT_CHANGE}%)${MD_COLOR_END} | ${ICON} |"
 done
