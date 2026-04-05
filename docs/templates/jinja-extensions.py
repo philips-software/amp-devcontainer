7afe757198b959229ebb8af5d1efbc45fecd79a4
rjaegers <45816308+rjaegers@users.noreply.github.com>
2026-04-05 14:45:10 +0000

    docs: process review comments

--- docs/templates/jinja-extensions.py
+++ docs/templates/jinja-extensions.py
@@ -1,6 +1,9 @@
-import datetime
+from datetime import datetime
 
 def strftime_filter(value, fmt):
-    return datetime.datetime.fromisoformat(value).strftime(fmt)
+    if value.endswith('Z'):
+        value = value[:-1] + '+00:00'
+
+    return datetime.fromisoformat(value).strftime(fmt)
 
 filters = { 'strftime': strftime_filter }
